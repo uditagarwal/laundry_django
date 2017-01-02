c92882da1f823be286173a49dcef0d7de4d7c30a
Udit Agarwal <udit.wal@gmail.com>
2017-01-02 23:29:09 +0530

    created user model and login and setup html

--- washd/users/models.py
+++ washd/users/models.py
@@ -13,10 +13,29 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    name = models.CharField(_('Name of User'), blank=True, max_length=255, null=True)
+    firstName = models.CharField(max_length=70, null=True)
+    lastName = models.CharField(max_length=70, null=True)
+    USER_TYPES = (
+        ('D', 'Dirty'),
+        ('W', 'Washr')
+    )
+    type = models.CharField(max_length=1, choices=USER_TYPES, null=True)
 
     def __str__(self):
         return self.username
 
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
+
+class Addresses(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200, null=True)
+    city = models.TextField(null=True)
+    state = models.TextField(null=True)
+    zipCode = models.TextField(null=True)
+    locationName = models.CharField(max_length=50, null=True)
+
+class PaymentAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(null=True)
